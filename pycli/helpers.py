d245b7e759602059afb00b3544abab5239848612
Raav <r.schravesande@wearetriple.com>
2024-08-05 09:58:11 +0200

    feat: added helpers

--- pycli/helpers.py
+++ pycli/helpers.py
@@ -2,16 +2,17 @@ import os
 import pathlib
 import tempfile
 import shutil
+import subprocess
 
 
 def read_env() -> "dict[str, str]":
     env_file = pathlib.Path(__file__).parent.parent.joinpath(".env")
-    
+
     if not env_file.exists():
-        return
-    
+        return dict()
+
     environment_variables = dict()
-    
+
     with env_file.open("r", encoding="utf-8") as fs:
         env_vars = fs.readlines()
     for env_var in env_vars:
@@ -20,11 +21,13 @@ def read_env() -> "dict[str, str]":
             continue
         key, value = env_var.split("=")
         environment_variables[key.strip()] = value.strip()
-    
+
     return environment_variables
+
 
 def load_env():
     os.environ.update(read_env())
+
 
 def print_with_glow(content: str):
     """
@@ -40,8 +43,56 @@ def print_with_glow(content: str):
 
     f = tempfile.TemporaryFile(delete=False)
     try:
-        f.write(content.encode('utf-8'))
+        f.write(content.encode("utf-8"))
         f.close()
         os.system(f"glow {f.name}")
     finally:
         pathlib.Path(f.name).unlink(missing_ok=True)
+
+
+def docker_deamon_available():
+    """
+    Returns True if docker daemon is available
+    """
+    try:
+        subprocess.run(["docker", "ps"], capture_output=True)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def get_ports_used_by_docker():
+    """
+    Returns a list of ports used by docker containers
+    """
+
+    ports = []
+    try:
+        result = subprocess.run(
+            ["docker", "ps", "--format", "{{.Ports}}"], capture_output=True
+        )
+        for line in result.stdout.decode("utf-8").split("\n"):
+            if not line:
+                continue
+            for port in line.split(","):
+                if port:
+                    binding = port.split("->")[0].strip()
+                    port = binding.replace("/tcp", "").split(":")[-1]
+                    if "-" in port:
+                        ports.extend(
+                            range(int(port.split("-")[0]), int(port.split("-")[1]) + 1)
+                        )
+                    else:
+                        ports.append(int(port))
+    except Exception as e:
+        print(e)
+    return list(sorted(set(ports)))
+
+
+def confirm(question: str) -> bool:
+    """
+    Prompts the user with a question and returns True if the user confirms
+    """
+    answer = input(f"{question} [y/n]: ")
+    return answer.strip().lower() in ("y", "yes")
